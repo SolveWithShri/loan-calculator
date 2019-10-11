79af528eb55ea5e40d03511715b91b7c375fe6f8
shrikant <s.adhav@easternenterprise.com>
2019-10-11 19:42:02 +0530

    Update wrong route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{
-  path: '',
-  redirectTo: 'calculator',
-  pathMatch: 'full'
-}, {
-  path: 'calculator',
-  loadChildren: './calculator/calculator.module#CalculatorModule'
-}];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'calculator',
+    pathMatch: 'full'
+  },
+  {
+    path: 'calculator',
+    loadChildren: './calculator/calculator.module#CalculatorModule'
+  },
+  {
+    path: '**',
+    redirectTo: 'calculator',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
